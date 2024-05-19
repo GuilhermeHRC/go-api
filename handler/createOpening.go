316a0a3102a0a2c7a479108bea862d84e9036453
guilhermehrc <jerrymoon12345@gmail.com>
2024-05-19 11:50:01 -0300

    feat: add createOpening logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,35 @@ package handler
 import (
 	"net/http"
 
+	"github.com/GuilhermeHRC/go-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "Create Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Failed to validate request: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Failed to create opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "Failed to create opening on database")
+		return
+	}
+
+	sendSucces(ctx, "CreateOpening", opening)
 }
--- router/routes.go
+++ router/routes.go
@@ -7,6 +7,9 @@ import (
 
 func InitializeRoutes(router *gin.Engine) {
 
+	// Initialize handler
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 	{
 		// Show opening
