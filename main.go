9dda0904544476536433c231df22e27bee98338a
guilhermehrc <jerrymoon12345@gmail.com>
2024-05-19 03:18:10 -0300

    feat: opening schema and custom logger

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"github.com/GuilhermeHRC/go-api/config"
 	"github.com/GuilhermeHRC/go-api/router"
 )
 
-func main() {
+var (
+	logger *config.Logger
+)
 
+func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Error initializing configs: %v", err)
+		return
+	}
 	// Initialize Router
 	router.Init()
 }
